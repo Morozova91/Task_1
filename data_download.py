abf5bd91cd9f951421e466ec3fe700fe7933a5ef
Юлия <denveroboogang1991@yandex.ru>
2024-12-31 18:29:49 +0300

    Задача №3. Реализовать функционал: Экспорт данных CSV

---
+++ data_download.py
@@ -0,0 +1,44 @@
+import yfinance as yf
+
+"""
+ data_download.py
+ Отвечает за загрузку данных об акциях.
+Содержит функции для извлечения данных об акциях из интернета и расчёта скользящего среднего.
+Получает исторические данные об акциях для указанного тикера и временного периода.
+Возвращает DataFrame с данными."""
+def fetch_stock_data(ticker, period):
+    stock = yf.Ticker(ticker)
+    data = stock.history(period=period)
+    return data
+
+"""Добавляет в DataFrame колонку со скользящим средним, рассчитанным на основе цен закрытия."""
+def add_moving_average(data, window_size=5):
+    data['Moving_Average'] = data['Close'].rolling(window=window_size).mean()
+    return data
+
+"""вычисляет и выводит среднюю цену закрытия акций за заданный период."""
+def calculate_and_display_average_price(data):
+    print(data['Close'].mean(axis=0))
+
+"""анализирует данные и уведомляет пользователя, 
+если цена акций колебалась более чем на заданный процент за период.
+Функция будет вычислять максимальное и минимальное значения цены закрытия и сравнивать разницу с заданным порогом. 
+Если разница превышает порог, пользователь получает уведомление."""
+def notify_if_strong_fluctuations(data, threshold=20):
+
+    min_price = data['Close'].min()
+    max_price = data['Close'].max()
+    price_change = ((max_price - min_price) / min_price) * 100
+
+    # Уведомляем пользователя, если колебания превышают порог
+    if price_change > threshold:
+        print(f"Уведомление: Колебания цены акций составили {price_change:.2f}%, что превышает порог в {threshold}%.")
+    else:
+        print(f"Колебания цены акций составили {price_change:.2f}%, что не превышает порог в {threshold}%.")
+
+
+
+
+
+
+
